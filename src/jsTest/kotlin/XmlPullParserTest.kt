42edceb2cf497f5d6a559fef53a68f6bb0d21f89
Lukundo Kileha <lkileha@gmail.com>
2021-02-05 13:06:37 +0300

    Added test cases for both xml and xhtml

--- src/jsTest/kotlin/XmlPullParserTest.kt
+++ src/jsTest/kotlin/XmlPullParserTest.kt
@@ -26,11 +26,55 @@ class XmlPullParserTest {
             "  </spine>\n" +
             "</package>\n"
 
+    private val XHTML_CONTENT = "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\" xml:lang=\"en\" lang=\"en\">\n" +
+            "<head>\n" +
+            "    <meta charset=\"utf-8\"/>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<nav epub:type=\"toc\" id=\"toc\">\n" +
+            "    <ol>\n" +
+            "        <li><a href=\"Page_1.xhtml\">Page 1</a></li>\n" +
+            "        <li><a href=\"Page_2.xhtml\">Page 2</a></li>\n" +
+            "        <li><a href=\"Page_3.xhtml\">Page 3</a></li>\n" +
+            "        <li><a href=\"Page_4.xhtml\">Page 4</a></li>\n" +
+            "        <li><a href=\"Page_5.xhtml\">Page 5</a></li>\n" +
+            "        <li><a href=\"Page_6.xhtml\">Page 6</a></li>\n" +
+            "        <li><a href=\"Acknowledgements.xhtml\">Acknowledgements</a></li>\n" +
+            "    </ol>\n" +
+            "</nav>\n" +
+            "</body>\n" +
+            "</html>"
+
     @Test
-    fun givenParsedContent_whenFirstNextIsCalled_ShouldBeDocumentRead(){
+    fun givenParsedXmlContent_whenFirstNextIsCalled_ShouldBeDocumentRead(){
         val xmlPullParser = UmXmlPullParser()
         xmlPullParser.setInput(XML_CONTENT)
-        assertEquals(0,xmlPullParser.next())
+        assertEquals(ParserEvent.START_DOCUMENT,xmlPullParser.next())
+    }
+
+    @Test
+    fun givenParsedXhtmlContent_whenFirstNextIsCalled_ShouldBeDocumentRead(){
+        val xmlPullParser = UmXmlPullParser()
+        xmlPullParser.setInput(XHTML_CONTENT)
+        assertEquals(ParserEvent.START_DOCUMENT,xmlPullParser.next())
+    }
+
+    @Test
+    fun givenParsedXmlContentAfterDocumentRead_whenNextIsCalled_ShouldBeTagRead(){
+        val xmlPullParser = UmXmlPullParser()
+        xmlPullParser.setInput(XML_CONTENT)
+        //read document
+        xmlPullParser.next()
+        assertEquals(ParserEvent.START_TAG,xmlPullParser.next())
+    }
+
+    @Test
+    fun givenParsedXhtmlContentAfterDocumentRead_whenNextIsCalled_ShouldBeTagRead(){
+        val xmlPullParser = UmXmlPullParser()
+        xmlPullParser.setInput(XHTML_CONTENT)
+        //read document
+        xmlPullParser.next()
+        assertEquals(ParserEvent.START_TAG,xmlPullParser.next())
     }
 
 }
