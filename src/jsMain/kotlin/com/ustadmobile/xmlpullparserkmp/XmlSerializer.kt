64efbc3ec215b0b1c2c97ffdbd75501e16f4c75b
Lukundo Kileha <lkileha@gmail.com>
2021-05-12 11:23:56 +0300

    Minor change

--- src/jsMain/kotlin/com/ustadmobile/xmlpullparserkmp/XmlSerializer.kt
+++ src/jsMain/kotlin/com/ustadmobile/xmlpullparserkmp/XmlSerializer.kt
@@ -1,21 +1,28 @@
 package com.ustadmobile.xmlpullparserkmp
 
 actual interface XmlSerializer {
+
     actual fun docdecl(dd: String)
+
     actual fun startDocument(encoding: String, standalone: Boolean)
+
     actual fun setPrefix(prefix: String, namespace: String)
+
     actual fun getPrefix(namespace: String?, generatePrefix: Boolean): String?
+
     actual fun startTag(namespace: String?, name: String): XmlSerializer
-    actual fun attribute(
-        namespace: String?,
-        name: String,
-        value: String
-    ): XmlSerializer
+
+    actual fun attribute(namespace: String?, name: String, value: String): XmlSerializer
 
     actual fun text(text: String): XmlSerializer
+
     actual fun cdsect(text: String)
+
     actual fun endTag(namespace: String?, name: String): XmlSerializer
+
     actual fun endDocument()
+
     actual fun entityRef(text: String)
+
     actual fun flush()
 }
